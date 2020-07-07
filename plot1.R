06a9b5d374b0fa8fdfc04d6598305d72764ab8ca
XinyuWei <66735945+XinyuW-hub@users.noreply.github.com>
2020-07-07 10:55:44 +0800

    commit plot1.R and export plot1.png

---
+++ plot1.R
@@ -0,0 +1,17 @@
+#Download,unzip and load the data
+if (!file.exists("./data")){dir.create("./data")}
+dataUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataUrl, destfile = "./data/EPC.zip")
+unzip("./data/EPC.zip")
+projectData <- read.table("household_power_consumption.txt", sep = ";", header = TRUE,
+                          na.strings = "?", nrows = 2075259)
+
+#Subset the data to cover only from 2007-2-1 and 2007-2-2
+selected <- subset(projectData, Date == "1/2/2007" | Date == "2/2/2007")
+selected$Date <- as.Date(selected$Date, format = "%d/%m/%Y")
+
+#create and export the first PNG plot
+hist(selected$Global_active_power, main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+png("plot1.png", width = 480, height = 480)
+dev.off()
